12b96c93ec389a3372ab2fb70a361555393a926f
MSI\Tc <timchu781015@hotmail.com>
2021-06-30 14:12:17 +0800

    implement Parcelable for FlightModel to pass flight data from FlightAdapter to FlightDetailActivity

--- app/src/main/java/com/tc/flightcentrequizzapp_android/FlightModel.kt
+++ app/src/main/java/com/tc/flightcentrequizzapp_android/FlightModel.kt
@@ -1,6 +1,9 @@
 package com.tc.flightcentrequizzapp_android
 
-data class FlightModel(
+import android.os.Parcel
+import android.os.Parcelable
+
+data class FlightModel (
         val airline_code: String,
         val arrival_airport: String,
         val arrival_city: String,
@@ -11,4 +14,45 @@ data class FlightModel(
         val flight_number: String,
         val id: Int,
         val scheduled_duration: String
-)
+) : Parcelable {
+        constructor(parcel: Parcel) : this(
+                parcel.readString().toString(),
+                parcel.readString().toString(),
+                parcel.readString().toString(),
+                parcel.readString().toString(),
+                parcel.readString().toString(),
+                parcel.readString().toString(),
+                parcel.readString().toString(),
+                parcel.readString().toString(),
+                parcel.readInt(),
+                parcel.readString().toString()
+        ) {
+        }
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+                parcel.writeString(airline_code)
+                parcel.writeString(arrival_airport)
+                parcel.writeString(arrival_city)
+                parcel.writeString(arrival_date)
+                parcel.writeString(departure_airport)
+                parcel.writeString(departure_city)
+                parcel.writeString(departure_date)
+                parcel.writeString(flight_number)
+                parcel.writeInt(id)
+                parcel.writeString(scheduled_duration)
+        }
+
+        override fun describeContents(): Int {
+                return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<FlightModel> {
+                override fun createFromParcel(parcel: Parcel): FlightModel {
+                        return FlightModel(parcel)
+                }
+
+                override fun newArray(size: Int): Array<FlightModel?> {
+                        return arrayOfNulls(size)
+                }
+        }
+}
